7cd709b7df4f03f6b6efe99ed578c24b852794ce
likuor <likuor0528@gmail.com>
2024-01-23 15:29:04 -0800

    add: 0123

---
+++ 2024/Easy/0123/index.js
@@ -0,0 +1,20 @@
+// FIXME
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  // if (n <= 2) {
+  //   return n;
+  // }
+  // return climbStairs(n - 1) + climbStairs(n - 2);
+  // climbStairs(2) = 2
+  // climbStairs(1) = 1
+  // climbStairs(3) = climbStairs(2) + climbStairs(1) = 2 + 1 = 3
+  // climbStairs(4) = climbStairs(3) + climbStairs(2) = 3 + 2 = 5
+};
+
+console.log(climbStairs(10));
+climbStairs(2);
+climbStairs(5);
