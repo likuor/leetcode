1fe380718c4eefea4fbd16d23e529e72423daa15
likuor <likuor0528@gmail.com>
2024-02-20 17:29:47 -0800

    add: 0220

---
+++ 2024/Easy/0220/index.js
@@ -0,0 +1,11 @@
+// TC O(n*m)
+// SC n(1)
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
