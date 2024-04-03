10ac9735c8653d96c0988ebea84ab02d38ec35d3
likuor <likuor0528@gmail.com>
2024-04-03 16:18:24 -0700

    add: 0403

--- 2024/Easy/0403/index.js
+++ 2024/Easy/0403/index.js
@@ -16,8 +16,28 @@ var isUgly = function (n) {
   return false;
 };
 
+// var isUgly = function (n) {
+//   if (n <= 0) return false;
+
+//   while (n != 1) {
+//     if (n % 2 === 0) {
+//       n /= 2;
+//     } else if (n % 3 === 0) {
+//       n /= 3;
+//     } else if (n % 5 === 0) {
+//       n /= 5;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
 console.log(isUgly(6));
 console.log(isUgly(1));
 console.log(isUgly(14));
 console.log(isUgly(0));
 console.log(isUgly(7));
+console.log(isUgly({ name: 12, email: 'test' }));
+console.log(isUgly({ name: 12, email: 'test' }));
