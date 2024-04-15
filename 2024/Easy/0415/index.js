f59a426ebc8276457a274c6dd31de048f2815d6e
likuor <likuor0528@gmail.com>
2024-04-15 15:57:03 -0700

    add: 0415

--- 2024/Easy/0415/index.js
+++ 2024/Easy/0415/index.js
@@ -6,8 +6,29 @@
  * @return {boolean}
  */
 var isPerfectSquare = function (num) {
-  return Number.isInteger(Math.sqrt(num));
+  return Number.isInteger(num ** 0.5);
+
+  // if (num < 1) {
+  //   return false;
+  // }
+  // if (num === 1) {
+  //   return true;
+  // }
+  // let left = 1;
+  // let right = num;
+  // while (left <= right) {
+  //   let mid = Math.floor((left + right) / 2);
+  //   if (mid * mid === num) {
+  //     return true;
+  //   } else if (mid * mid > num) {
+  //     right = mid - 1;
+  //   } else if (mid * mid < num) {
+  //     left = mid + 1;
+  //   }
+  // }
+  // return false;
 };
 
 console.log(isPerfectSquare(16));
-console.log(isPerfectSquare(14));
+// console.log(isPerfectSquare(25));
+// console.log(isPerfectSquare(14));
