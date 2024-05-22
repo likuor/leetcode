7821a7dcabecabc0a0d457dda76443a01277b428
likuor <likuor0528@gmail.com>
2024-05-22 16:24:36 -0700

    add: 0522

--- 2024/Easy/May/0522/index.js
+++ 2024/Easy/May/0522/index.js
@@ -5,10 +5,26 @@
  * @param {number} n
  * @return {number}
  */
+// var fib = function (n) {
+//   if (n === 0) return 0;
+//   if (n < 3) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// TC O(n)
+// SC O(1)
 var fib = function (n) {
   if (n === 0) return 0;
-  if (n < 3) return 1;
-  return fib(n - 1) + fib(n - 2);
+  if (n === 1) return 1;
+
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 };
 
 console.log(fib(2));
